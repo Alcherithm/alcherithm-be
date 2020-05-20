d9650be742582dfd7c6ec8c273bd1b7a30b52bd9
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-20 16:31:25 -0700

    add auth to user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const schema = new mongoose.Schema({
   fistName: {
@@ -18,7 +20,7 @@ const schema = new mongoose.Schema({
     type: String,
     required: true
   },
-  userCohort: String,
+  cohort: String,
   avatar: String,
   sessionDates: [String],
   sessionDurations: [String],
@@ -39,6 +41,61 @@ schema.virtual('solutions', {
   foreignField: 'userId'
 });
 
+// get date when user signed up
 schema.virtual('date').get(function() {
-  return this.updatedAt.toISOString().split('T')[0];
+  return this.createdAt.toISOString().split('T')[0];
 });
+
+// TO DO: get counts for user's completed and attempted challenges
+
+// send error if email already exists
+schema.post('save', function(error, doc, next) {
+  if(error.code === 100) return next(new Error('Email already taken'));
+  next(error);
+});
+
+// hash password
+schema.virtual('password').set(function(password) {
+  const hash = bcrypt.hashSync(password, 8);
+  this.passwordHash = hash;
+});
+
+// authorize user: confirm email and password
+schema.statics.authorize = async function({ email, password }) {
+  const user = await this.findOne({ email });
+  if(!user) {
+    const error = new Error('Invalid email/password');
+    error.status = 401;
+    throw error;
+  }
+
+  const matchingPasswords = await bcrypt.compare(password, user.passwordHash);
+  if(!matchingPasswords) {
+    const error = new Error('Invalid email/password');
+    error.status = 401;
+    throw error;
+  }
+
+  return user;
+};
+
+// find user by token
+schema.statics.findByToken = function(token) {
+  try {
+    const { payload } = jwt.verify(token, process.env.APP_SECRET);
+    return Promise.resolve(this.hydrate(payload));
+  } catch(e) {
+    return Promise.reject(e);
+  }
+};
+
+// send token for authorized user
+schema.methods.authToken = function() {
+  const token = jwt.sign({ payload: this.toJSON() }, process.env.APP_SECRET, {
+    expiresIn: '24h'
+  });
+
+  return token;
+};
+
+module.exports = mongoose.model('User', schema);
