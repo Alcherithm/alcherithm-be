4bf802b3a0b8204a6c98d2ec204b2af13fcc9a51
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-21 01:23:44 -0700

    write failing test for user verification

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,4 +1,4 @@
-require('../db/data-helpers');
+const { getAgent } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -23,10 +23,11 @@ describe('auth routes', () => {
           updatedAt: expect.any(String),
           signUpDate: expect.any(String),
           sessionDates: expect.any(Array),
-          sessionDurations: expect.any(Array),
+          sessionDurations: expect.any(Array)
         });
       });
   });
+  
   it('logs in a user', () => {
     return request(app)
       .post('/api/v1/auth/login')
@@ -45,7 +46,26 @@ describe('auth routes', () => {
           updatedAt: expect.any(String),
           signUpDate: expect.any(String),
           sessionDates: expect.any(Array),
-          sessionDurations: expect.any(Array),
+          sessionDurations: expect.any(Array)
+        });
+      });
+  });
+
+  it('verifies a logged in user', () => {
+    return getAgent()
+      .get('/api/v1/auth/verify')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          firstName: 'Charlotte',
+          lastName: 'Katze',
+          email: 'charlotte@domain.com',
+          cohort: 'Winter 2020',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          signUpDate: expect.any(String),
+          sessionDates: expect.any(Array),
+          sessionDurations: expect.any(Array)
         });
       });
   });
