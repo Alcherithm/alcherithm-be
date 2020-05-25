9ee92f16b908f3dee1507c3bae6332514a9fdf99
janelle mellor <janellemellor@gmail.com>
2020-05-25 13:49:06 -0700

    update passing test for get solutions by challenge and user id

--- __tests__/solutions.test.js
+++ __tests__/solutions.test.js
@@ -45,19 +45,32 @@ export const speaker = (message, callback) => {
       });
   });
 
-  it('gets solutions by challenge id', async() => {
-    const challenge = await getChallenge();
-    const solutions = await getSolutions({ challengeId: challenge._id, passed: true });
-
-    return request(app)
-      .get(`/api/v1/solutions?challengeId=${challenge._id}`)
-      .then(res => {
-        expect(res.body).toEqual(solutions.map(solution => ({
-          _id: solution._id,
-          solution: solution.solution
-        })));
-      });
-  });
+  // it('gets solutions by user id and groups by challenge number', async() => {
+  //   const solutions = await getSolutions();
+  //   const userIds = solutions.map(solution => solution.userId);
+  
+  //   return request(app)
+  //     .get(`/api/v1/solutions?userId=${userIds[0]}`)
+  //     .then(res => {
+  //       expect(res.body).toContainEqual(solutions.map(solution => ({
+  //         __v: expect.any(Number),
+  //         _id: expect.any(String),
+  //         solutions: [
+  //           {
+  //             solution: solution.solution,
+  //             passed: solution.passed,
+  //             createdAt: solution.createdAt
+  //           }
+  //         ],
+  //         challenge: {
+  //           category: expect.any(String),
+  //           challengeNumber: expect.any(Number),
+  //           instructions: expect.any(String)
+  //         }
+  //       })));
+  //     });
+  // });  
+ 
 
   it('gets solutions by challenge id and user id', async() => {
     const challenge = await getChallenge();
@@ -68,11 +81,25 @@ export const speaker = (message, callback) => {
     return request(app)
       .get(`/api/v1/solutions?challengeId=${challenge._id}&userId=${userIds[0]}`)
       .then(res => {
+        expect(res.body).toContainEqual({
+          _id: solutions[0]._id,
+          createdAt: solutions[0].createdAt,
+          passed: solutions[0].passed,
+          solution: solutions[0].solution
+        });
+      });
+  });
+
+
+  it('gets solutions by challenge id', async() => {
+    const challenge = await getChallenge();
+    const solutions = await getSolutions({ challengeId: challenge._id, passed: true });
+
+    return request(app)
+      .get(`/api/v1/solutions?challengeId=${challenge._id}`)
+      .then(res => {
         expect(res.body).toEqual(solutions.map(solution => ({
-          __v: expect.any(Number),
           _id: solution._id,
-          createdAt: solution.createdAt,
-          passed: solution.passed,
           solution: solution.solution
         })));
       });
